06bc634f439bb68ea19542888e57537fabf1840d
Dan Suh <kaehops@gmail.com>
2018-08-25 21:25:14 +0900

    Model skeleton

---
+++ model.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+
+class ResNet34(nn.Module):
+    def __init__(self, mode='A'):
+        super().__init__()
+        # input : 224 x 224
+        nn.Conv2d()
+
+    def forward(self, x):
+        pass
+
+
+if __name__ == '__main__':
+    pass
