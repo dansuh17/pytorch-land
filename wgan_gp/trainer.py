02b25690e291f182619c2dc6fbbe21910d48062b
Dan Suh <kaehops@gmail.com>
2019-02-06 02:13:48 +0900

    Implemented WGAN-GP

---
+++ wgan_gp/trainer.py
@@ -0,0 +1,162 @@
+import random
+from typing import Dict
+import torch
+from torch import optim
+import torchvision
+from .wgan_gp import WganGpDiscriminator
+from .wgan_gp import WganGpGenerator
+from datasets.img_popular import LSUNLoaderMaker
+from base_trainer import NetworkTrainer
+from base_trainer import ModelInfo
+from base_trainer import TrainStage
+
+
+class WGANTrainer(NetworkTrainer):
+    """Trainer for Wasserstein GAN with Gradient Penalty.
+    Gulrajani et al. - "Improved Training of Wasserstein GANs" (2017)
+    """
+    def __init__(self, config: dict):
+        print('Configuration:')
+        print(config)
+
+        # parse configs
+        self.batch_size = config['batch_size']
+        self.input_dim = config['input_dim']
+        self.total_epoch = config['epoch']
+        self.display_imgs = config['display_imgs']
+        self.grad_penalty_coeff = config['grad_penalty_coeff']
+
+        # create a loader maker
+        loader_maker = LSUNLoaderMaker(data_root='data_in', batch_size=self.batch_size)
+
+        # create models
+        generator = WganGpGenerator(input_dim=self.input_dim)
+        discriminator = WganGpDiscriminator()
+        models = {
+            'WGANGP_G': ModelInfo(
+                model=generator,
+                input_size=(self.input_dim, ),
+                metric='loss_g',
+            ),
+            'WGANGP_D': ModelInfo(
+                model=discriminator,
+                input_size=(self.input_dim, ),
+                metric='loss_d',
+            )
+        }
+
+        # create optimizers
+        self.lr_init = config['lr_init']
+        optimizers = {
+            'g_optim': optim.Adam(
+                generator.parameters(), lr=self.lr_init, betas=(0.5, 0.999)),
+            'd_optim': optim.Adam(
+                discriminator.parameters(), lr=self.lr_init, betas=(0.5, 0.999))
+        }
+
+        # criteria = None
+        super().__init__(
+            models, loader_maker, None, optimizers, epoch=self.total_epoch, num_devices=4)
+
+        self.epoch = 0
+
+    def run_step(self,
+                 model: Dict[str, ModelInfo],
+                 criteria,
+                 optimizer,
+                 input_,
+                 train_stage: TrainStage,
+                 *args, **kwargs):
+        imgs, _ = input_
+
+        # parse models
+        generator = model['WGANGP_G'].model
+        discriminator = model['WGANGP_D'].model
+
+        g_optim, d_optim = optimizer['g_optim'], optimizer['d_optim']
+
+        ones = torch.ones((self.batch_size, 1)).to(self.device)
+        minus_ones = ones * -1.0
+
+        ###############
+        ### train G ###
+        ###############
+        noise_vec = torch.randn((self.batch_size, self.input_dim, 1, 1))
+        generated_img = generator(noise_vec)
+        g_loss = discriminator(generated_img)
+
+        if train_stage == TrainStage.TRAIN:
+            g_optim.zero_grad()
+            g_loss.backward(ones)
+            g_optim.step()
+
+        ###############
+        ### train D ###
+        ###############
+        noise_vec = torch.randn((self.batch_size, self.input_dim, 1, 1))
+
+        # loss function - discriminator acts like a 'critic'
+        d_loss_real = discriminator(imgs)
+        d_loss_fake = discriminator(generator(noise_vec).detach())
+
+        ### calculate the gradient penalty (GP)
+        # linear-interpolated input
+        alpha = random.random()
+        img_interp = (imgs - generated_img) * alpha + generated_img
+        img_interp = img_interp.detach().requires_grad_()  # set requires_grad=True to store the grad value
+        score_img_interp = discriminator(img_interp)
+        score_img_interp.backward(torch.ones(img_interp.size()))
+        gp = self.grad_penalty_coeff * torch.pow(img_interp.grad.norm(dim=1) - 1, 2).mean()
+
+        if train_stage == TrainStage.TRAIN:
+            d_optim.zero_grad()
+            # backward process must be provided with the tensor w.r.t.
+            # the gradient will be calculated, since these are not scalar valued tensors
+            d_loss_real += gp
+            d_loss_real.backward(ones)
+            d_loss_fake.backward(minus_ones)
+            d_optim.step()
+
+        d_loss = d_loss_real - d_loss_fake + gp
+
+        # collect outputs as return values
+        outputs = (
+            generated_img,
+            imgs,
+            gp,
+        )
+        losses = (g_loss, d_loss, d_loss_real, d_loss_fake)
+        return outputs, losses
+
+    @staticmethod
+    def make_performance_metric(input_, output, loss):
+        return {
+            'gp': output[2].item(),
+            'g_loss': torch.mean(loss[0]).item(),
+            'd_loss': torch.mean(loss[1]).item(),
+            'd_loss_real': torch.mean(loss[2]).item(),
+            'd_loss_fake': torch.mean(loss[3]).item(),
+        }
+
+    def pre_epoch_finish(self, input_, output, metric_manager, train_stage: TrainStage):
+        if train_stage == TrainStage.VALIDATE:
+            gen_img = output[0]
+            real_img = output[1]
+            self.add_generated_image(
+                gen_img, nrow=self.display_imgs, height=None, width=None, name='gen')
+            self.add_generated_image(
+                real_img, nrow=self.display_imgs, height=None, width=None, name='real')
+
+    def add_generated_image(self, imgs, nrow, height, width, name: str):
+        grid = torchvision.utils.make_grid(imgs[:nrow, :], nrow=nrow, normalize=True)
+        self.writer.add_image('{}/{}'.format(self.epoch, name), grid, self.train_step)
+
+
+if __name__ == '__main__':
+    import json
+    with open('wgan/config.json', 'r') as configf:
+        config = json.loads(configf.read())
+
+    trainer = WGANTrainer(config)
+    trainer.fit()
+    trainer.cleanup()
---
+++ wgan_gp/wgan_gp.py
@@ -0,0 +1,114 @@
+"""
+"""
+import torch
+from torch import nn
+
+
+class WganGpGenerator(nn.Module):
+    """Generator model of WGAN-GP.
+    """
+    def __init__(self, input_dim: int):
+        super().__init__()
+        # create model with a stack of 5 convolutional layers
+        self.net = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels=input_dim, out_channels=1024, kernel_size=4,
+                stride=1, padding=0, bias=False),  # output : (1024, 4, 4)
+            nn.BatchNorm2d(1024),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(1024, 512, 4, 2, 1, bias=False),  # (512, 8, 8)
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),  # (256, 16, 16)
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),  # (128, 32, 32)
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(128, 3, 4, 2, 1, bias=False),  # (3, 64, 64)
+            nn.Tanh(),
+        )
+
+        self.apply(self.init_weights)
+
+    def forward(self, z):
+        return self.net(z)
+
+    @staticmethod
+    def init_weights(m):
+        if isinstance(m, nn.Linear):
+            nn.init.kaiming_uniform_(m.weight)
+            nn.init.constant_(m.bias, 0)
+
+
+class WganGpDiscriminator(nn.Module):
+    """Discriminator model of WGAN-GP.
+    """
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            # input : (3, 64, 64)
+            nn.Conv2d(
+                in_channels=3, out_channels=64, kernel_size=4,
+                stride=2, padding=1, bias=False),  # output: (64, 32, 32)
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(64, 128, 4, 2, 1, bias=False),  # output : (128, 16, 16)
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(128, 256, 4, 2, 1, bias=False),  # output : (256, 8, 8)
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(256, 512, 4, 2, 1, bias=False),  # (512, 4, 4)
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(512, 1, 4, 1, 0, bias=False),  # (1, 1, 1)
+        )
+
+        self.apply(self.init_weights)
+
+    def forward(self, x):
+        return self.net(x).view(-1, 1)
+
+    @staticmethod
+    def init_weights(m):
+        if isinstance(m, nn.Linear):
+            nn.init.kaiming_uniform_(m.weight)
+            nn.init.constant_(m.bias, 0)
+
+
+if __name__ == '__main__':
+    # artificially create 4D tensor (representing a batch of 1D vector having size 100)
+    z = torch.randn((3, 100, 1, 1))
+    g = WganGpGenerator(100)
+    print(g)
+    output = g(z)
+    print(output.size())
+
+    d = WganGpDiscriminator()
+    print(d)
+    disc_out = d(output)
+    print(disc_out)
+    print(disc_out.size())
+    disc_out.backward(torch.ones(disc_out.size()))
+
+    # testing calculation of GP
+    import random
+    alpha = random.random()
+    real_img = torch.randn((3, 3, 64, 64))
+    img_interp = (real_img - output) * alpha + output
+    img_interp = img_interp.detach().requires_grad_()  # set requires_grad=True to store the grad value
+    score_img_interp = d(img_interp)
+    score_img_interp.backward(torch.ones(score_img_interp.size()))
+
+    grad_pen = 10 * torch.pow(img_interp.grad.norm(2, dim=1) - 1, 2).mean()
+    # out = output.detach().requires_grad_()
+    # disc_out2 = d(out)
+    # disc_out2.backward(torch.ones(disc_out2.size()))
+    # print(out.grad)
+    print(grad_pen)
+    res = score_img_interp + grad_pen
+    res.backward()
