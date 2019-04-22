7821346f49190fa14eb999d6b9aaf4c0bc4cfa2b
Dan Suh <kaehops@gmail.com>
2019-04-22 10:36:14 +0900

    Removed autograd anomaly detection

--- alexnet/trainer.py
+++ alexnet/trainer.py
@@ -4,7 +4,7 @@ Implementation of training of alexnet.
 import operator
 from typing import Dict
 import torch
-from torch import optim, nn, autograd
+from torch import optim, nn
 from torch.optim.optimizer import Optimizer
 from .alexnet import AlexNet
 from base_trainer import NetworkTrainer, ModelInfo, TrainStage
@@ -25,7 +25,6 @@ class AlexNetTrainer(NetworkTrainer):
         self.lr = config['lr']
         self.num_devices = config['num_devices']
         self.img_dim = 227
-        autograd.set_detect_anomaly(True)
 
         loadermaker = ImageNetLoaderMaker(self.data_root, self.batch_size, num_workers=4, img_dim=self.img_dim)
         self.input_size = (3, self.img_dim, self.img_dim)
@@ -70,10 +69,9 @@ class AlexNetTrainer(NetworkTrainer):
 
         # update parameters
         if train_stage == TrainStage.TRAIN:
-            with autograd.detect_anomaly():
-                opt.zero_grad()
-                loss.backward()
-                opt.step()
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
 
         outputs = (out_features, target_batch)
         losses = (loss, )
