48bb246334c15347e3b9712481bf109bc24d6bae
Dan Suh <kaehops@gmail.com>
2019-07-22 23:21:44 +0900

    simple setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytorch-land',
+    version='0.1.0',
+    description='pytorch-land for happy deep learning',
+    long_description='',
+    author='deNsuh',
+    author_email='kaehops@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+)
