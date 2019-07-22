25c023ed7ecb5b92e043d53997945c4838dabf82
Dan Suh <kaehops@gmail.com>
2019-07-23 02:52:25 +0900

    Version bumpup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytorch-land',
-    version='0.1.0',
+    version='0.1.1',
     description='pytorch-land for happy deep learning',
     long_description='',
     author='dansuh17',
