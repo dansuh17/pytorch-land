9f6d3241ebc067a3fc3e1efd42dca3ae5c7e03a2
Dan Suh <kaehops@gmail.com>
2019-07-23 18:07:46 +0900

    project long description added

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
+import pathlib
 from setuptools import setup, find_packages
+
+ROOT_PATH = pathlib.Path(__file__).parent
+README_TEXT = (ROOT_PATH / 'README.md').read_text()
 
 setup(
     name='pytorch-land',
     version='0.1.1',
     description='pytorch-land for happy deep learning',
-    long_description='',
+    long_description=README_TEXT,
+    long_description_content_type='text/markdown',
     author='dansuh17',
     author_email='kaehops@gmail.com',
     url='https://github.com/dansuh17/pytorch-land',
