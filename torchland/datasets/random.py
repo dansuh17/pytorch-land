34b44d4f6ce0f9eb75d44bf547c028a062e6a995
Dan Suh <kaehops@gmail.com>
2019-11-14 13:27:50 +0900

    random value dataset implemented

--- torchland/datasets/random.py
+++ torchland/datasets/random.py
@@ -1,12 +1,20 @@
-from torch.utils.data import Dataset, DataLoader
+from typing import Tuple
+import torch
+from torch.utils.data import Dataset
 
 
 class RandomDataset(Dataset):
-    def __init__(self, shape, size: int):
+    """
+    Dataset that generates random-valued tensor having certain shape provided.
+    """
+    def __init__(self, shape: Tuple[int], size: int):
         super().__init__()
+        self.shape = shape
+        self.size = size
 
     def __getitem__(self, item):
-        pass
+        # generate random tensor
+        return torch.randn(*self.shape)
 
     def __len__(self):
-        pass
+        return self.size
